9de9d7e645cd8a039f5b4d9c855fed317880c1d7
Lopez1710 <69539735+Lopez1710@users.noreply.github.com>
2021-09-24 18:07:21 -0600

    todo

---
+++ Precencial/Precencial/Controllers/PersonaController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Precencial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Precencial.Controllers
+{
+    public class PersonaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CamposViewModel campos)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index",campos);
+            }
+            else
+            {
+                return RedirectToAction("Exito");
+            }
+            
+        }
+        public IActionResult Exito()
+        {
+            return View();
+        }
+    }
+}
---
+++ Precencial/Precencial/Models/CamposViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Precencial.Models
+{
+    public class CamposViewModel
+    {
+        [Required(ErrorMessage ="Campo Obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage ="Campo Obligatorio")]
+        [Range(0,100,ErrorMessage ="Edad Erronea")]
+        public int Edad { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string Sexo { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string Educacion { get; set; }
+    }
+}
